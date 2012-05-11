9bc10814f693ebb73a3460ea6035425e251dd929
Deepwinter <deepwinter@winterroot.net>
2012-05-11 16:07:25 -0700

    Waypoint: working on projects chooser

---
+++ Source/Views and Controllers/ProjectsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProjectsTableViewController.h
+//  Rhus
+//
+//  Created by Deep Winter on 5/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProjectsTableViewController : UITableViewController
+
+@end
